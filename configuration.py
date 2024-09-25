539d5f4e150c6d2bd9f01849ff0a40486f4db28b
Arturo <argacompanies@gmail.com>
2024-09-25 02:07:37 -0600

    actualizo url

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-af9ff801-efdd-48ea-a6e3-479fafa2dfed.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-52302234-05ab-4489-96a5-76d8b6039be3.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users/" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits/" # Almacena la ruta para crear un kit en esta variable
